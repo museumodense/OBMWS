f07acb846c69011f732711c7489b3f60c7f49427
andvo <andvo@odense.dk>
2016-11-22 14:03:34 +0100

    Update default WSConstants

--- Src/OBMWS/core/com/WSConstants.cs
+++ Src/OBMWS/core/com/WSConstants.cs
@@ -964,4 +964,44 @@ namespace OBMWS
             }
         }
     }
+
+    public class WSConstantsDefault : WSClientMeta
+    {
+        public WSConstantsDefault(HttpContext context) : base(context) { }
+        protected override void InitServer() { SecurityMap = new Dictionary<string, WSSecurityMeta>(); }
+
+        public override string SrcBinaryKey { get { return ""; } }
+
+        public override void InnerDispose() { }
+
+        public override void loadStaticSources() { }
+        public override WSEmail CreateEmail(string _Subject, WSEmailLines _Lines, string _ToAddress, string _FromAddress = null)
+        {
+            return new IEmail(_Subject, _Lines, _ToAddress, _FromAddress);
+        }
+        public override void RegError(Type caller, Exception e, ref WSStatus status, string errorMsg = null)
+        {
+            try
+            {
+
+            }
+            catch (Exception) { }
+        }
+    }
+    public class IEmail : WSEmail
+    {
+        public IEmail(string _Subject, WSEmailLines _Lines, string _ToAddress, string _FromAddress = null) : base(_Subject, _Lines, _ToAddress, _FromAddress) { }
+        public override WSInstitutionMeta Institution { get { return new IInstitutionMeta(); } }
+    }
+    public class IInstitutionMeta : WSInstitutionMeta
+    {
+        public override string Phone { get { return "+45 65 51 46 01"; } }
+        public override string Fax { get { return "+45 65 90 86 00"; } }
+        public override string Email { get { return "museum@odense.dk"; } }
+        public override string Title { get { return "DWS Service"; } }
+        public override Uri Url { get { return new Uri("http://museum.odense.dk/"); } }
+        public override Uri IconUrl { get { return new Uri("http://museum.odense.dk/media/11472973/odense_logo_small.png"); } }
+        public override WSAddress Address { get { return _Address; } }
+        private WSAddress _Address = new WSAddress() { City = "Odense C", StreetAddress = "Overgade 48", ZIP = "DK-5000" };
+    }
 }
